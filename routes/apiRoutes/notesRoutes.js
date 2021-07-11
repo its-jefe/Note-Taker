f8b7c2ad01e67e58277fad87e945cff6fea9d63c
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-07-11 13:09:17 -0400

    learning

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
+const { createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/notes');
 
 // notes are available at api/notes in JSON 
 router.get('/notes', (req, res) => {
   let results = notes;
   res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+
 });
 
 module.exports = router;
