0b477fac999315d83f05c45b24eeb6b6b175c6ef
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-07-11 11:44:03 -0400

    test

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,37 +1,10 @@
 const router = require('express').Router();
-const { 
-  getNotes,
-  createNote,
-  deleteNote
-} = require('../../lib/notes');
-// note handling functions
-
-const { notes } = require('../../db/notes');
+const { } = require('../../lib/notes');
+const { animals } = require('../../db/notes');
 
 router.get('/notes', (req, res) => {
-  let results = notes;
+  let results = animals;
   res.json(results);
-});
-
-router.get('/notes/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-router.post('/animals', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
-
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
 });
 
 module.exports = router;
